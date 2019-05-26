3189301ff821e3d98bbea3916a1355522d127acd
Newrichman <nursultansaidahmatov@gmail.com>
2019-05-26 06:27:07 +0600

    сменить на турецкий

--- src/application/textToSpeech.java
+++ src/application/textToSpeech.java
@@ -19,47 +19,49 @@ public class textToSpeech {
 	 */
 	public textToSpeech() {
 		
-		String str = jTextArea1.getText();
-                str = str.replaceAll("к", "k");
-                str = str.replaceAll("ж", "zh");
-                str = str.replaceAll("т", "tj");
+                String str = jTextArea1.getText();
+                str = str.replaceAll("a", "a");
+                str = str.replaceAll("ә", "ä");
+                str = str.replaceAll("б", "b");
                 str = str.replaceAll("в", "v");
                 str = str.replaceAll("г", "g");
+                str = str.replaceAll("ғ", "ğ");
                 str = str.replaceAll("д", "d");
-                str = str.replaceAll("л", "l");
-                str = str.replaceAll("м", "m");
-                str = str.replaceAll("н", "n");
-                str = str.replaceAll("п", "p");
-                str = str.replaceAll("р", "r");
-                str = str.replaceAll("с", "s");
-                str = str.replaceAll("ф", "f");
-                str = str.replaceAll("х", "h");
-                str = str.replaceAll("һ", "h");
+                str = str.replaceAll("е", "e");
+                str = str.replaceAll("ё", "yo");
+                str = str.replaceAll("ж", "j");
                 str = str.replaceAll("з", "z");
-                str = str.replaceAll("ц", "c");
-                str = str.replaceAll("ч", "ch");
-                str = str.replaceAll("ш", "sh");
-                str = str.replaceAll("щ", "sch");
-                str = str.replaceAll("й", "j");
-                str = str.replaceAll("қ", "qh");
-                str = str.replaceAll("ғ", "gh");
-                str = str.replaceAll("ң", "ng");
-                
-                str = str.replaceAll("ө", "uoe");
-                str = str.replaceAll("е", "je");
-                str = str.replaceAll("у", "uu");
-                str = str.replaceAll("ы", "y");
-                str = str.replaceAll("и", "yi");
-                str = str.replaceAll("ү", "u");
-                str = str.replaceAll("ұ", "uy");
-                str = str.replaceAll("ю", "ju");
-                str = str.replaceAll("о", "o");
-                str = str.replaceAll("э", "e");
-                str = str.replaceAll("е", "je");
-                str = str.replaceAll("ё", "jo");
-                str = str.replaceAll("і", "ji");
-                str = str.replaceAll("а", "a");
-                
+                str = str.replaceAll("и", "ï");
+            str = str.replaceAll("й", "y");
+            str = str.replaceAll("к", "k");
+            str = str.replaceAll("қ", "q");
+            str = str.replaceAll("л", "ll");
+            str = str.replaceAll("м", "m");
+            str = str.replaceAll("н", "n");
+            str = str.replaceAll("ң", "ñ");
+            str = str.replaceAll("о", "o");
+            str = str.replaceAll("ө", "ö");
+            str = str.replaceAll("п", "p");
+            str = str.replaceAll("р", "r");
+            str = str.replaceAll("с", "s");
+            str = str.replaceAll("т", "t");
+            str = str.replaceAll("у", "w");
+            str = str.replaceAll("ұ", "u");
+            str = str.replaceAll("ү", "ü");
+            str = str.replaceAll("ф", "f");
+            str = str.replaceAll("х", "h");
+            str = str.replaceAll("һ", "h");
+            str = str.replaceAll("ц", "ts"); // important: must consider "ts" at beginning of word
+            str = str.replaceAll("ч", "ç");
+            str = str.replaceAll("ш", "ş");
+            str = str.replaceAll("щ", "şş");
+            str = str.replaceAll("ъ", "\'");
+            str = str.replaceAll("ы", "ı");
+            str = str.replaceAll("і", "i");
+            str = str.replaceAll("ь", "\'");
+            str = str.replaceAll("э", "é");
+            str = str.replaceAll("ю", "yu");
+            str = str.replaceAll("я", "ya");
                 System.out.println("Text to speech: [" + jTextArea1.getText() + " " + str + "]\n");
 		speak(str);
 	}
